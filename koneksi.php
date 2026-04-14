44918ef57e7eee24dde0843c43c396c09c5dad27
Qoonitah <rahmahazmillah03@gmail.com>
2026-04-14 15:05:48 +0700

    commit pertama

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,14 @@
 <?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "db_kampus";
+
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+
+if (!koneksi) {
+  die("koneksi gagal: ". mysqli_connect_error());
+}
+
 echo "coba aja";
 ?>
